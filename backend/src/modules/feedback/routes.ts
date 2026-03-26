a981f3641fbff7ec3f47f4ebbcc77c32c535a6bc
Noori-Amasha <nooriamashaed@gmail.com>
2026-03-26 11:58:19 +0530

    feat(feedback):implement backend for feedback module.

---
+++ backend/src/modules/feedback/routes.ts
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+import { auth } from '../../middleware/auth.js';
+import { allowRoles } from '../../middleware/role.js';
+import { ROLES } from '../../core/constants.js';
+import * as controller from './controller.js';
+import {
+  validateCreateFeedback,
+  validateFeedbackId,
+  validateGetFeedbacks,
+  validateUpdateStatus,
+  validateUpdateMyFeedback,
+  validateDeleteMyFeedback
+} from './validation.js';
+
+const router = Router();
+router.get('/public', controller.getApprovedFeedbacks);
+
+router.post('/',auth,allowRoles(ROLES.CUSTOMER),validateCreateFeedback,controller.createFeedback,);
+
+router.get('/my/:orderId',auth,allowRoles(ROLES.CUSTOMER),controller.getMyFeedbackForOrder,);
+router.patch('/my/:id',auth,allowRoles(ROLES.CUSTOMER),validateUpdateMyFeedback,controller.updateMyFeedbackController,);
+router.delete('/my/:id',auth,allowRoles(ROLES.CUSTOMER),validateDeleteMyFeedback,controller.deleteMyFeedbackController,);
+router.get('/my', auth, allowRoles(ROLES.CUSTOMER), controller.getMyAllFeedbacksController);
+
+router.get('/stats',auth,allowRoles(ROLES.ADMIN),controller.getFeedbackStats,);
+
+router.get('/',auth,allowRoles(ROLES.ADMIN),validateGetFeedbacks, controller.getAllFeedbacks,);
+router.get('/:id',auth,allowRoles(ROLES.ADMIN),validateFeedbackId,controller.getFeedbackById,);
+router.patch('/:id/status',auth,allowRoles(ROLES.ADMIN),validateUpdateStatus,controller.updateFeedbackStatus,);
+
+export default router;
