1225aecce7206084887cac969d62f04f2f9c4c4d
Virul Akbo De Silva <waviruldesilva@gmail.com>
2026-04-16 01:56:56 +0530

    add the actual barchart in dashboard

--- backend/src/modules/finance/financeRoutes.ts
+++ backend/src/modules/finance/financeRoutes.ts
@@ -2,8 +2,10 @@ import { Router } from 'express';
 import * as financeController from './financeController.js';
 
 const router = Router();
+
 router.get('/summary', financeController.getFinanceSummary);
 router.get('/revenue', financeController.getAllRevenues);
+router.get('/revenue/monthly', financeController.getMonthlyRevenue);
 router.get('/expense', financeController.getAllExpenses);
 router.post('/revenue', financeController.addRevenue);
 router.post('/expense', financeController.addExpense);
